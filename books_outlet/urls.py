67f34e8692aa334522f4dc2cd5f0f34b64c483a3
Mohamed Talaat <mohamed.talaat.info@gmail.com>
2026-02-13 01:24:21 +0200

    done

--- books_outlet/urls.py
+++ books_outlet/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('book/<slug:slug>/', views.book_details, name='book_info')
 ]
--- books_outlet/views.py
+++ books_outlet/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse, Http404
 from .models import Books
 # Create your views here.
 
 
 def index(request):
-    entire_books = Books.objects.all()
+    entire_books = Books.objects.all().order_by("-rating")
     return render(request, "books_outlet/books_list.html", {
         "books": entire_books
+    })
+
+
+def book_details(request, slug):
+    book = get_list_or_404(Books, slug=slug)[0]
+    title = book.title
+    brief = book.brief
+    author = book.author.all()
+    countries = book.published_countries.all()
+    rating = book.rating
+    is_bestseller = book.is_bestseller
+    print(author)
+    return render(request, "books_outlet/book_details.html", {
+        "title": title,
+        "brief": brief,
+        "author": author,
+        "countries": countries,
+        "rating": rating,
+        "is_bestseller": is_bestseller
+        
     })
